3e198ec9f6b9bd7caf8c1ca8759acd9895c429b2
Matt <matt@mjman.net>
2022-04-13 13:30:20 +1000

    Ignore `Declaration of` PHP warnings  (legacy module support).

--- src/phproad/core/exceptions.php
+++ src/phproad/core/exceptions.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Phpr;
 
 use Phpr;
@@ -24,9 +25,9 @@ class Exception extends \Exception
      * Creates a new Phpr\Exception instance.
      *
      * @param string $Message Message of exception.
-     * @param int    $Code    Code of exception.
+     * @param int $Code Code of exception.
      */
-    public function __construct( $Message = null, $Code = 0 )
+    public function __construct($Message = null, $Code = 0)
     {
         // Call the parent constructor
         //
@@ -34,14 +35,14 @@ class Exception extends \Exception
 
         // Log the exception
         //
-        if (Phpr::$errorLog !== null ) {
+        if (Phpr::$errorLog !== null) {
             try {
                 $result = Phpr::$errorLog->logException($this);
-                if(is_array($result)) {
+                if (is_array($result)) {
                     $this->log_id = $result['id'] ?? null;
                     $this->log_status = $result['status'] ?? null;
                 }
-            } catch ( Exception $ex ) {
+            } catch (Exception $ex) {
                 // Prevent the looping
             }
         }
@@ -50,11 +51,11 @@ class Exception extends \Exception
     /**
      * Formats a trace string for display in HTML or text format.
      *
-     * @param  Exception                                                         $Exception Specifies an exception to format the trace for.
-     * @param  boolean @Html Indicates whether the result must be in HTML format.
+     * @param Exception $Exception Specifies an exception to format the trace for.
+     * @param boolean @Html Indicates whether the result must be in HTML format.
      * @return string
      */
-    public static function formatTrace( $Exception, $Html = true )
+    public static function formatTrace($Exception, $Html = true)
     {
         $result = null;
 
@@ -64,18 +65,18 @@ class Exception extends \Exception
 
         // Begin the event list
         //
-        if ($Html ) {
+        if ($Html) {
             $result .= "<ol>\n<li>";
         }
 
         $newLineChar = $Html ? "</li>\n<li>" : "\n";
 
-        foreach ( $TraceInfo as $index => $Event ) {
-            $functionName = ( isset($Event['class']) && strlen($Event['class']) ) ? $Event['class'] . '->' . $Event['function'] : $Event['function'];
+        foreach ($TraceInfo as $index => $Event) {
+            $functionName = (isset($Event['class']) && strlen($Event['class'])) ? $Event['class'] . '->' . $Event['function'] : $Event['function'];
 
             // Do not include the handlers to the trace
             //
-            if ($functionName == 'Phpr\SysErrorHandler' || $functionName == 'Phpr\SysExceptionHandler' ) {
+            if ($functionName == 'Phpr\SysErrorHandler' || $functionName == 'Phpr\SysExceptionHandler') {
                 continue;
             }
 
@@ -85,24 +86,24 @@ class Exception extends \Exception
             // Prepare the argument list
             //
             $args = null;
-            if (isset($Event['args']) && count($Event['args']) ) {
+            if (isset($Event['args']) && count($Event['args'])) {
                 $args = self::_formatTraceArguments($Event['args'], $Html);
             }
 
-            if (! is_null($file) ) {
+            if (!is_null($file)) {
                 $result .= sprintf('%s(%s) in %s, line %s', $functionName, $args, $file, $line);
             } else {
                 $result .= $functionName . "($args)";
             }
 
-            if ($index < $lastIndex ) {
+            if ($index < $lastIndex) {
                 $result .= $newLineChar;
             }
         }
 
         // End the event list
         //
-        if ($Html ) {
+        if ($Html) {
             $result .= "</li></ol>\n";
         }
 
@@ -112,51 +113,51 @@ class Exception extends \Exception
     /**
      * Prepares a function or method argument list for display in HTML or text format
      *
-     * @param  array                                                             &$arguments A list of the function or method arguments
-     * @param  boolean @Html Indicates whether the result must be in HTML format.
+     * @param array                                                             &$arguments A list of the function or method arguments
+     * @param boolean @Html Indicates whether the result must be in HTML format.
      * @return string
      */
-    public static function _formatTraceArguments( &$arguments, $Html = true )
+    public static function _formatTraceArguments(&$arguments, $Html = true)
     {
         $argsArray = array();
-        foreach ( $arguments as $argument ) {
+        foreach ($arguments as $argument) {
             $arg = null;
 
-            if (is_array($argument) ) {
+            if (is_array($argument)) {
                 $items = array();
 
-                foreach ( $argument as $k => $v ) {
-                    if (is_array($v) ) {
+                foreach ($argument as $k => $v) {
+                    if (is_array($v)) {
                         $value = 'array(' . count($v) . ')';
-                    } elseif (is_object($v) ) {
+                    } elseif (is_object($v)) {
                         $value = 'object(' . get_class($v) . ')';
-                    } elseif ($v === null ) {
+                    } elseif ($v === null) {
                         $value = 'null';
-                    } elseif (is_integer($argument) ) {
+                    } elseif (is_integer($argument)) {
                         $value = $v;
                     } else {
-                        $value = "'" . ( $Html ? Html::encode($v) : $v ) . "'";
+                        $value = "'" . ($Html ? Html::encode($v) : $v) . "'";
                     }
 
                     $items[] = $k . ' => ' . $value;
                 }
 
-                if (count($items) ) {
+                if (count($items)) {
                     $arg = 'array(' . count($argument) . ') [' . implode(', ', $items) . ']';
                 } else {
                     $arg = 'array(0)';
                 }
-            } elseif (is_object($argument) ) {
+            } elseif (is_object($argument)) {
                 $arg = 'object(' . get_class($argument) . ')';
-            } elseif ($argument === null ) {
+            } elseif ($argument === null) {
                 $arg = 'null';
-            } elseif (is_integer($argument) ) {
+            } elseif (is_integer($argument)) {
                 $arg = $argument;
             } else {
-                $arg = "'" . ( $Html ? Html::encode($argument) : $argument ) . "'";
+                $arg = "'" . ($Html ? Html::encode($argument) : $argument) . "'";
             }
 
-            if ($Html ) {
+            if ($Html) {
                 $arg = '<span style="color: #398999">' . $arg . '</span>';
             }
 
@@ -239,56 +240,56 @@ class HttpException extends ApplicationException
     public $http_code;
 
     protected static $status_messages = array(
-    100 => 'Continue',
-    101 => 'Switching Protocols',
-    200 => 'OK',
-    201 => 'Created',
-    202 => 'Accepted',
-    203 => 'Non-Authoritative Information',
-    204 => 'No Content',
-    205 => 'Reset Content',
-    206 => 'Partial Content',
-    300 => 'Multiple Choices',
-    301 => 'Moved Permanently',
-    302 => 'Found',
-    303 => 'See Other',
-    304 => 'Not Modified',
-    305 => 'Use Proxy',
-    307 => 'Temporary Redirect',
-    400 => 'Bad Request',
-    401 => 'Unauthorized',
-    402 => 'Payment Required',
-    403 => 'Forbidden',
-    404 => 'Not Found',
-    405 => 'Method Not Allowed',
-    406 => 'Not Acceptable',
-    407 => 'Proxy Authentication Required',
-    408 => 'Request Time-out',
-    409 => 'Conflict',
-    410 => 'Gone',
-    411 => 'Length Required',
-    412 => 'Precondition Failed',
-    413 => 'Request Entity Too Large',
-    414 => 'Request-URI Too Large',
-    415 => 'Unsupported Media Type',
-    416 => 'Requested range not satisfiable',
-    417 => 'Expectation Failed',
-    500 => 'Internal Server Error',
-    501 => 'Not Implemented',
-    502 => 'Bad Gateway',
-    503 => 'Service Unavailable',
-    504 => 'Gateway Time-out',
-    505 => 'HTTP Version not supported',
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        307 => 'Temporary Redirect',
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Time-out',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Request Entity Too Large',
+        414 => 'Request-URI Too Large',
+        415 => 'Unsupported Media Type',
+        416 => 'Requested range not satisfiable',
+        417 => 'Expectation Failed',
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Time-out',
+        505 => 'HTTP Version not supported',
     );
 
     /**
      * Creates a new Phpr\Exception instance.
      *
-     * @param int    $http_code HTTP code.
-     * @param string $message   message of the exception.
-     * @param int    $code      Code of exception.
+     * @param int $http_code HTTP code.
+     * @param string $message message of the exception.
+     * @param int $code Code of exception.
      */
-    public function __construct( $http_code, $message = null, $code = 0 )
+    public function __construct($http_code, $message = null, $code = 0)
     {
         $this->http_code = $http_code;
         parent::__construct($message, $code);
@@ -297,10 +298,10 @@ class HttpException extends ApplicationException
     /**
      * Outputs the error message along with a corresponding HTTP header.
      *
-     * @param boolean $stop           Stop script execution after outputting the message.
+     * @param boolean $stop Stop script execution after outputting the message.
      * @param boolean $output_message Indicates whether the error message should be outputted before the.
      */
-    public function output( $stop = true, $output_message = true )
+    public function output($stop = true, $output_message = true)
     {
         self::output_custom($this->http_code, $this->getMessage(), $stop, $output_message);
     }
@@ -308,27 +309,27 @@ class HttpException extends ApplicationException
     /**
      * Outputs custom HTTP header with a message
      *
-     * @param int     $http_code      HTTP code.
-     * @param string  $message        message of the exception.
-     * @param boolean $stop           Stop script execution after outputting the message.
+     * @param int $http_code HTTP code.
+     * @param string $message message of the exception.
+     * @param boolean $stop Stop script execution after outputting the message.
      * @param boolean $output_message Indicates whether the error message should be outputted before the.
      */
-    public static function output_custom( $http_code, $message, $stop = true, $output_message = true )
+    public static function output_custom($http_code, $message, $stop = true, $output_message = true)
     {
         $header_text = 'HTTP/1.1 ' . $http_code . ' ';
-        if (isset(self::$status_messages[ $http_code ]) ) {
-            $header_text .= self::$status_messages[ $http_code ];
+        if (isset(self::$status_messages[$http_code])) {
+            $header_text .= self::$status_messages[$http_code];
         } else {
             $header_text .= $message;
         }
 
         header($header_text);
 
-        if ($output_message ) {
+        if ($output_message) {
             echo $message;
         }
 
-        if ($stop ) {
+        if ($stop) {
             die;
         }
     }
@@ -348,11 +349,11 @@ class PhpException extends SystemException
      * Creates a new Phpr\Exception instance.
      *
      * @param string $Message Message of exception.
-     * @param int    $Type    Type of the PHP error.
-     * @param string $File    Source filename.
-     * @param string $Line    Source line.
+     * @param int $Type Type of the PHP error.
+     * @param string $File Source filename.
+     * @param string $Line Source line.
      */
-    public function __construct( $Message, $Type, $File, $Line )
+    public function __construct($Message, $Type, $File, $Line)
     {
         $this->file = $File;
         $this->line = $Line;
@@ -370,15 +371,15 @@ class PhpException extends SystemException
         $result = null;
 
         $errorNames = array(
-        E_WARNING      => 'PHP Warning',
-        E_NOTICE       => 'PHP Notice',
-        E_STRICT       => 'PHP Strict Error',
-        E_USER_ERROR   => 'PHP User Error',
-        E_USER_WARNING => 'PHP User Warning',
-        E_USER_NOTICE  => 'PHP User Notice',
+            E_WARNING => 'PHP Warning',
+            E_NOTICE => 'PHP Notice',
+            E_STRICT => 'PHP Strict Error',
+            E_USER_ERROR => 'PHP User Error',
+            E_USER_WARNING => 'PHP User Warning',
+            E_USER_NOTICE => 'PHP User Notice',
         );
 
-        $result = isset($errorNames[ $this->code ]) ? $errorNames[ $this->code ] : 'PHP Error';
+        $result = isset($errorNames[$this->code]) ? $errorNames[$this->code] : 'PHP Error';
         return $result . ': ' . $this->getMessage();
     }
 }
@@ -437,14 +438,12 @@ class ExecutionException extends SystemException
         $call_stack = array_reverse($call_stack);
         $this->call_stack = $call_stack;
         $this->code_line = $line;
-        $this->location_desc = '"'.$this->document_name().'" ('.$this->document_type().')';
+        $this->location_desc = '"' . $this->document_name() . '" (' . $this->document_type() . ')';
 
-        if ($load_line_from_stack)
-        {
+        if ($load_line_from_stack) {
             $trace = $this->getTrace();
             $count = count($trace);
-            if ($count)
-            {
+            if ($count) {
                 $this->code_line = $trace[0]['line'];
             }
         }
@@ -480,23 +479,29 @@ class ExecutionException extends SystemException
  */
 function SysErrorHandler($errno, $errstr, $errfile, $errline)
 {
-    // Throw the PHP Exception if it is listed in the ERROR_REPORTING configuration value
-    //
 
-    if (Phpr::$config !== null && ( Phpr::$config->get(
-        'ERROR_REPORTING',
-        E_ALL
-    ) & $errno ) && ( error_reporting() & $errno ) 
-    ) {
+    $errorReporting = E_ALL;
+    if (Phpr::$config !== null) {
+        $errorReporting = Phpr::$config->get('ERROR_REPORTING', $errorReporting);
+    }
+
+    // Only throw a PHP Exception if it is listed in the ERROR_REPORTING configuration value
+    $throwError = ($errorReporting & $errno) && (error_reporting() & $errno);
+
+    //Ignore Declaration of warnings (legacy module support)
+    if (strpos($errstr, 'Declaration of') === 0) {
+        $throwError = false;
+    }
+
+    if ($throwError) {
         throw new PhpException($errstr, $errno, $errfile, $errline);
-    } else {
-        // Otherwise throw and catch the exception to log it
-        //
-        try {
-            throw new PhpException($errstr, $errno, $errfile, $errline);
-        } catch ( Exception $e ) {
-            // Do nothing
-        }
+    }
+
+    try {
+        //Log and catch
+        throw new PhpException($errstr, $errno, $errfile, $errline);
+    } catch (Exception $e) {
+        // Do nothing
     }
 }
 
@@ -509,8 +514,7 @@ function SysExceptionHandler($exception)
     try {
         Phpr::$response->openErrorPage($exception);
     } catch (\Exception $e) {
-        print get_class($e) . " thrown within the exception handler. Message: " . $e->getMessage(
-            ) . " on line " . $e->getLine() . " of " . $e->getFile();
+        print get_class($e) . " thrown within the exception handler. Message: " . $e->getMessage() . " on line " . $e->getLine() . " of " . $e->getFile();
     }
 }
 
