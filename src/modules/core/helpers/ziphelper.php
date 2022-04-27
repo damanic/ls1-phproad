e056ff6977d9869f172bc6e6c83c4738585f7488
Matt <matt@mjman.net>
2022-04-27 15:15:09 +1000

    parent compat

--- src/modules/core/helpers/ziphelper.php
+++ src/modules/core/helpers/ziphelper.php
@@ -14,8 +14,8 @@ class ZipHelper extends Zip
      * @deprecated
      * Use Phpr\Zip
      */
-    public static function unzip($path, $archivePath, $no_set_permissions = false)
+    public static function unzip($path, $archivePath, $no_set_permissions = false, $replace_files = true)
     {
-        Zip::unzip($path, $archivePath, true, !$no_set_permissions);
+        Zip::unzip($path, $archivePath, $replace_files, !$no_set_permissions);
     }
 }
