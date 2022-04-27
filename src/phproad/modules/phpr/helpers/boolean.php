c8b3e89d93750430af10b4f7f2e1193457c713a5
Matt <matt@mjman.net>
2022-04-27 14:32:15 +1000

    moore booley

--- src/phproad/modules/phpr/helpers/boolean.php
+++ src/phproad/modules/phpr/helpers/boolean.php
@@ -4,39 +4,47 @@ namespace Phpr;
 /**
  * PHPR Boolean helper
  *
- * This class contains functions that may be useful for working with booleans.
+ * This class contains functions that may be useful for working with boolean strings.
  */
 class Boolean
 {
+    /**
+     * Converts any given value to a BOOL value
+     * @param string $value
+     * @return bool
+     */
     public static function from($value)
     {
         if (is_string($value)) {
-            return self::from_string($value);
+            return self::fromString($value);
         } else {
-            return (boolean)$value;
+            return (bool)$value;
         }
     }
 
-    public static function fromString($str)
+    /**
+     * Convert a string to BOOL
+     * @param string $str
+     * @return bool
+     */
+    public static function fromString(string $str)
     {
-        $str = trim($str);
-
         if ($str == true) {
             return true;
         } else {
-            if ($str == 'y') {
+            $trueValues = array(
+                '1',
+                'enabled',
+                'y',
+                'yes',
+                'active',
+                'true'
+            );
+            $str = trim($str);
+            if (in_array($str, $trueValues)) {
                 return true;
-            } else {
-                if ($str == 'yes') {
-                    return true;
-                } else {
-                    if ($str == 'true') {
-                        return true;
-                    }
-                }
             }
         }
-
         return false;
     }
 }
