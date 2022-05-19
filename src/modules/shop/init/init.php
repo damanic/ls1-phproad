9bafea3478bd045a4638418731c80c11ba2e4eef
Matt <matt@mjman.net>
2022-05-19 10:01:44 +1000

    autoload vendors on init

--- src/modules/shop/init/init.php
+++ src/modules/shop/init/init.php
@@ -1,3 +1,4 @@
 <?php
+require_once(PATH_APP . '/modules/shop/vendor/autoload.php');
 Phpr::$classLoader->add_module_directory('shipping_types');
 Phpr::$classLoader->add_module_directory('payment_types');
