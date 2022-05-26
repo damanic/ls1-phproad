12d210d4b738a485b2465d13dba8f2a1863a0d96
Matt <matt@mjman.net>
2022-05-26 15:16:08 +1000

    Refactor init

--- src/modules/shop/init/init.php
+++ src/modules/shop/init/init.php
@@ -2,3 +2,7 @@
 require_once(PATH_APP . '/modules/shop/vendor/autoload.php');
 Phpr::$classLoader->add_module_directory('shipping_types');
 Phpr::$classLoader->add_module_directory('payment_types');
+Phpr::$classLoader->add_module_directory('currency_converters');
+Phpr::$classLoader->add_module_directory('price_rule_actions');
+Phpr::$classLoader->add_module_directory('price_rule_conditions');
+Phpr::$classLoader->add_module_directory('price_rule_conditions/condition_base');
