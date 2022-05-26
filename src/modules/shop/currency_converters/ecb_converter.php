63e820136a7db554a922fb834b3bd5674e720d28
Matt <matt@mjman.net>
2022-05-26 15:31:56 +1000

    namespace currency_converters

---
+++ src/modules/shop/currency_converters/ecb_converter.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Shop;
+
+use Phpr\SystemException;
+
+/**
+ * Currency converter based on European Central Bank
+ * free XML currency rate feed
+ */
+class Ecb_Converter extends CurrencyConverterBase
+{
+    const feed_address = 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml';
+
+    /**
+     * Returns information about the currency converter.
+     * @return array Returns array with two keys: name and description
+     * array('name'=>'My converter name', 'description'=>'My converter description')
+     */
+    public function get_info()
+    {
+        return array(
+            'name' => 'European Central Bank',
+            'description' =>
+                'This converter uses the free currency exchange rate feed 
+                provided by European Central Bank (www.ecb.int)'
+        );
+    }
+
+    /**
+     * Returns exchange rate for two currencies
+     * This method must be implemented in a specific currency converter.
+     * @param \Db\ActiveRecord $parameters_base Active Record object to read parameters from
+     * @param string $from_currency Specifies 3 character ISO currency code (e.g. USD) to convert from.
+     * @param string $to_currency Specifies a currency code to convert to
+     * @return float
+     */
+    public function get_exchange_rate($host_obj, $from_currency, $to_currency)
+    {
+        $from_currency = trim(strtoupper($from_currency));
+        $to_currency = trim(strtoupper($to_currency));
+
+        $feed_content = null;
+        try {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, self::feed_address);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_POST, false);
+            $feed_content = curl_exec($ch);
+            curl_close($ch);
+        } catch (\Exception $ex) {
+        }
+
+        if (!strlen($feed_content)) {
+            throw new SystemException('Error loading European Central Bank feed.');
+        }
+
+        $doc = new \DOMDocument('1.0');
+        $doc->loadXML($feed_content);
+        $xPath = new \DOMXPath($doc);
+        $xPath->registerNamespace('ns', "http://www.ecb.int/vocabulary/2002-08-01/eurofxref");
+
+        if ($from_currency == 'EUR') {
+            $from_rate = 1;
+        } else {
+            $from_rate = $xPath->query("//gesmes:Envelope/ns:Cube/ns:Cube/ns:Cube[@currency='$from_currency']");
+            if (!$from_rate->length) {
+                throw new SystemException('Currency rate for ' . $from_currency . ' not found');
+            }
+
+            $from_rate = $from_rate->item(0)->getAttribute('rate');
+        }
+
+        if ($to_currency == 'EUR') {
+            $to_rate = 1;
+        } else {
+            $to_rate = $xPath->query("//gesmes:Envelope/ns:Cube/ns:Cube/ns:Cube[@currency='$to_currency']");
+            if (!$to_rate->length) {
+                throw new SystemException('Currency rate for ' . $to_currency . ' not found');
+            }
+
+            $to_rate = $to_rate->item(0)->getAttribute('rate');
+        }
+
+        if (!is_numeric($to_rate) || !is_numeric($from_rate)) {
+            throw new SystemException('Invalid currency rates received');
+        }
+
+        if (($to_rate <= 0) || ($from_rate <= 0)) {
+            throw new SystemException('Invalid currency rates received');
+        }
+
+        return $to_rate / $from_rate;
+    }
+}
---
+++ src/modules/shop/currency_converters/yahoo_converter.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Shop;
+
+use Phpr\SystemException;
+
+/**
+ * Currency converter based on Yahoo
+ * free currency exchange rate service
+ */
+class Yahoo_Converter extends CurrencyConverterBase
+{
+    const feed_address = 'http://finance.yahoo.com/d/quotes.csv?f=l1d1t1&s=%s%s=X';
+
+    /**
+     * Returns information about the currency converter.
+     * @return array Returns array with two keys: name and description
+     * array('name'=>'My converter name', 'description'=>'My converter description')
+     */
+    public function get_info()
+    {
+        return array(
+            'name' => 'Yahoo (Discontinued)',
+            'description' =>
+                'The service has been discontinued. 
+                For all future markets and equities data research, please refer to finance.yahoo.com.'
+        );
+    }
+
+    /**
+     * Returns exchange rate for two currencies
+     * This method must be implemented in a specific currency converter.
+     * @param \Db\ActiveRecord $parameters_base Active Record object to read parameters from
+     * @param string $from_currency Specifies 3 character ISO currency code (e.g. USD) to convert from.
+     * @param string $to_currency Specifies a currency code to convert to
+     * @return float
+     */
+    public function get_exchange_rate($host_obj, $from_currency, $to_currency)
+    {
+        $from_currency = trim(strtoupper($from_currency));
+        $to_currency = trim(strtoupper($to_currency));
+
+        $response = null;
+        try {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, sprintf(self::feed_address, $from_currency, $to_currency));
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_POST, false);
+            $feed_content = curl_exec($ch);
+            curl_close($ch);
+        } catch (\Exception $ex) {
+        }
+
+        if (!strlen($feed_content)) {
+            throw new SystemException('Error loading Yahoo currency exchange feed.');
+        }
+
+        $data = explode(',', $feed_content);
+        if (count($data) < 2) {
+            throw new SystemException('Yahoo currency exchange rate service has returned invalid data');
+        }
+
+        $rate = $data[0];
+        if (!is_numeric($rate) || $rate <= 0) {
+            throw new SystemException('Yahoo currency exchange rate service has returned invalid data');
+        }
+        return $rate;
+    }
+}
