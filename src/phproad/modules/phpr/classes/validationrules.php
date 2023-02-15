c3029490011de74bd0d07d7ef78a89e200cef366
Matt <matt@mjman.net>
2023-02-15 11:08:57 +1100

    Update URL validation

--- src/phproad/modules/phpr/classes/validationrules.php
+++ src/phproad/modules/phpr/classes/validationrules.php
@@ -830,10 +830,7 @@ class ValidationRules
             return true;
         }
 
-        $result = preg_match(
-            "~^(http|https|ftp|ssh|sftp|etc)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|localhost|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=_\-]+))*$~",
-            mb_strtolower($Value)
-        ) ? true : false;
+        $result = filter_var($Value, FILTER_VALIDATE_URL);
 
         if (!$result) {
             $Message = strlen($CustomMessage) ? $CustomMessage : sprintf(
