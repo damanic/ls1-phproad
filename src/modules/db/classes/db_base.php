c75fa9d68a94388e2b6b2ccf57b1ca9645ad9520
damanic <matt@mjman.net>
2024-03-18 17:02:48 +1100

    Use driver escape method in SQL prepare and quote functions.

--- src/modules/db/classes/db_base.php
+++ src/modules/db/classes/db_base.php
@@ -2,6 +2,8 @@
 
 class Db_Base extends Phpr_Validatable
 {
+    protected $_driver = null;
+
     public function prepare($sql, $bind = null)
     {
         // split into text and params
@@ -52,19 +54,27 @@ class Db_Base extends Phpr_Validatable
             return implode(', ', $value);
         }
 
-        //			if ($value == '*')
-        //				return $value;
-
         if ($value instanceof Phpr_DateTime) {
             $value = $value->toSqlDateTime();
         }
 
-        if (!strlen($value)) {
-            return 'null';
+        if (is_bool($value)) {
+            return $value ? '1' : '0';
         }
 
-        $result = str_replace("\\", '\\\\', $value);
-        $result = str_replace("'", "\'", $result);
-        return "'" . $result . "'";
+        if (is_null($value)) {
+            return 'NULL';
+        }
+
+        $value = (string) $value;
+        if ($this->_driver) {
+            $value = $this->_driver->escape($value);
+        } else {
+            $search = ["\\", "\0", "\n", "\r", "'", "\"", "\x1a"];
+   	        $replace = ["\\\\", "\\0", "\\n", "\\r", "\\'", "\\\"", "\\Z"];
+	        $value = str_replace($search, $replace, $value);
+        }
+
+        return "'" . $value . "'";
     }
 }
